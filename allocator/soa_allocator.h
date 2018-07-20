c9298ea29c263b4776954e503884abf9a5662bf5
Matthias Springer <me@matthiasspringer.de>
2018-07-19 18:49:36 -0700

    update

--- allocator/soa_allocator.h
+++ allocator/soa_allocator.h
@@ -34,22 +34,6 @@ __forceinline__ __device__ unsigned warp_id()
     return ret;
 }
 
-// Copied from: https://gist.github.com/pabigot/7550454
-template <typename T>
-__DEV__ T rotl (T v, unsigned int b)
-{
-  static_assert(std::is_integral<T>::value, "rotate of non-integral type");
-  static_assert(! std::is_signed<T>::value, "rotate of signed type");
-  constexpr unsigned int num_bits {std::numeric_limits<T>::digits};
-  static_assert(0 == (num_bits & (num_bits - 1)), "rotate value bit length not power of two");
-  constexpr unsigned int count_mask {num_bits - 1};
-  const unsigned int mb {b & count_mask};
-  using promoted_type = typename std::common_type<int, T>::type;
-  using unsigned_promoted_type = typename std::make_unsigned<promoted_type>::type;
-  return ((unsigned_promoted_type{v} << mb)
-          | (unsigned_promoted_type{v} >> (-mb & count_mask)));
-}
-
 
 template<typename T, int N, int Field, int Offset>
 class SoaField {
