9de98028e0939ddab1addee677a9cebc531e1956
Matthias Springer <me@matthiasspringer.de>
2018-08-01 09:58:41 +0900

    AOS allocation.

--- wa-tor/aos/wator_soa.cu
+++ wa-tor/aos/wator_soa.cu
@@ -10,8 +10,8 @@
 #define SPAWN_THRESHOLD 4
 #define ENERGY_BOOST 4
 #define ENERGY_START 2
-#define GRID_SIZE_X 400
-#define GRID_SIZE_Y 300
+#define GRID_SIZE_X 2048
+#define GRID_SIZE_Y 1024
 
 #define OPTION_SHARK_DIE true
 #define OPTION_SHARK_SPAWN true
