8b27ecfbd6c53b26a06cfbb1861bbba44b6647ac
Matthias Springer <me@matthiasspringer.de>
2018-08-01 09:29:57 +0900

    More scaling.

--- build_run_scaling.sh
+++ build_run_scaling.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-for i in `seq 0 256 8192`
+for i in `seq 0 256 16384`
 do
   echo "Running ${i}"
-  /usr/local/cuda/bin/nvcc -std=c++11 -O3 -use_fast_math wa-tor/aos/wator.cu -I. -I/home/matthias/halloc/install/include -dc -arch compute_61 -o wator.o -DGRID_SIZE_X=256 -DGRID_SIZE_Y=${i}
-/usr/local/cuda/bin/nvcc -std=c++11 -O3 -use_fast_math -arch compute_61 -L/home/matthias/halloc/install/lib -lhalloc -lSDL2 -lSDL2_gfx -o wator wator.o
+#  /usr/local/cuda/bin/nvcc -std=c++11 -O3 -use_fast_math wa-tor/aos/wator.cu -I. -I/home/matthias/halloc/install/include -dc -arch compute_61 -o wator.o -DGRID_SIZE_X=256 -DGRID_SIZE_Y=${i}
+#/usr/local/cuda/bin/nvcc -std=c++11 -O3 -use_fast_math -arch compute_61 -L/home/matthias/halloc/install/lib -lhalloc -lSDL2 -lSDL2_gfx -o wator wator.o
 #  /usr/local/cuda/bin/nvcc -std=c++11 -O3 -use_fast_math wa-tor/aos/wator_soa.cu -I. -arch compute_61 -DGRID_SIZE_X=256 -DGRID_SIZE_Y=${i}
-  ./wator >> bench_scaling_halloc.csv
-  cat bench_scaling_halloc.csv
+  /usr/local/cuda/bin/nvcc -std=c++11 -O3 -use_fast_math wa-tor/aos/wator.cu -I. -arch compute_61 -I mallocmc_copy/src/include -DGRID_SIZE_X=256 -DGRID_SIZE_Y=${i}
+  ./a.out >> bench_scaling_mallocmc.csv
+  cat bench_scaling_mallocmc.csv
 done
