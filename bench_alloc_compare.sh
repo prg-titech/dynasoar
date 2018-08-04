ec12d4cc9122f0a674f00838a765eed6d9b4a81e
Matthias Springer <me@matthiasspringer.de>
2018-08-04 09:42:38 +0900

    Alloc data.

--- bench_alloc_compare.sh
+++ bench_alloc_compare.sh
@@ -15,4 +15,18 @@ do
   echo "SOA"
   cat bench_alloc_compare_soa.csv
 
+
+  /usr/local/cuda/bin/nvcc -std=c++11 -DNUM_ALLOCS=${i} -DALLOC_SIZE=16 -O3 -use_fast_math microbench/linux_scalability.cu -I. -arch compute_61
+  ./a.out >> bench_alloc_compare_cuda.csv
+
+  echo "CUDA"
+  cat bench_alloc_compare_cuda.csv
+
+
+  /usr/local/cuda/bin/nvcc -std=c++11 -DNUM_ALLOCS=${i} -DALLOC_SIZE=16 -O3 -use_fast_math microbench/linux_scalability_halloc.cu -I. -I/home/matthias/halloc/install/include -dc -arch compute_61 -o wator.o
+  /usr/local/cuda/bin/nvcc -std=c++11 -O3 -use_fast_math -arch compute_61 -L/home/matthias/halloc/install/lib -lhalloc -o wator wator.o
+  ./wator >> bench_alloc_compare_halloc.csv
+echo "HALLOC"
+cat bench_alloc_compare_halloc.csv
+
 done
