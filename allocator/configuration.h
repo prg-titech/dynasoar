c60c858cff924fdf86a87d8e287c699c05ffcb0e
Matthias Springer <me@matthiasspringer.de>
2018-10-01 12:15:06 +0900

    Enforce object order.

--- allocator/configuration.h
+++ allocator/configuration.h
@@ -3,6 +3,6 @@
 
 // Data section begins after 128 bytes. This leaves enough space for bitmaps
 // and other data structures in blocks.
-static const int kBlockDataSectionOffset = 128;
+static const int kBlockDataSectionOffset = 64;
 
 #endif  // ALLOCATOR_AOS_CONFIGURATION_H
