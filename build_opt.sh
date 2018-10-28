deeb4653679d28df28242e0672b597a29690c3c1
Matthias Springer <me@matthiasspringer.de>
2018-10-28 18:51:49 +0900

    Change directory structure.

--- build_opt.sh
+++ build_opt.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/usr/local/cuda/bin/nvcc -std=c++11 -DNDEBUG -DHEAP_SIZE=500 -O3 -use_fast_math wa-tor/soa/wator.cu -DGRID_SIZE_Y=3300 -I. -arch compute_61 --expt-relaxed-constexpr
+/usr/local/cuda/bin/nvcc -std=c++11 -DNDEBUG -DHEAP_SIZE=500 -O3 -use_fast_math example/wa-tor/soa/wator.cu -DGRID_SIZE_Y=3300 -I. -arch compute_61 --expt-relaxed-constexpr
