b8f5b441f58d59547de607cfe256e0940fdf98fa
Matthias Springer <me@matthiasspringer.de>
2019-01-16 17:05:52 +0900

    Update.

--- allocator/configuration.h
+++ allocator/configuration.h
@@ -4,6 +4,12 @@
 // Active defragmentation support.
 #define OPTION_DEFRAG
 
+// Defragmentation factor.
+// 1 -- 50% -- Merge 1 block into 1 block
+// 2 -- 66% -- Merge 2 blocks into 1 block
+// 3 -- 75% -- Merge 3 blocks into 1 block
+static const int kDefragFactor = 2;
+
 // Data section begins after 128 bytes. This leaves enough space for bitmaps
 // and other data structures in blocks.
 static const int kBlockDataSectionOffset = 64;
