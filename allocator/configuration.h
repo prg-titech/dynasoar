90d901da63328a905b52a55e5ab40d24debf73e7
Matthias Springer <me@matthiasspringer.de>
2019-01-27 22:07:16 +0900

    Update.

--- allocator/configuration.h
+++ allocator/configuration.h
@@ -9,7 +9,7 @@
 // 2 -- 66% -- Merge 2 blocks into 1 block
 // 3 -- 75% -- Merge 3 blocks into 1 block
 // leq_threshold = kDefragFactor / (kDefragFactor + 1)
-static const int kDefragFactor = 5;
+static const int kDefragFactor = 9;
 
 // Data section begins after 128 bytes. This leaves enough space for bitmaps
 // and other data structures in blocks.
