0269dd3e6363a8bef27900f5731acef99eb7ce1d
Matthias Springer <me@matthiasspringer.de>
2019-03-30 14:38:44 +0900

    Increase Barnes-Hut problem size.

--- example/barnes-hut-dynamic/soa/configuration.h
+++ example/barnes-hut-dynamic/soa/configuration.h
@@ -4,10 +4,10 @@
 static const float kDistThreshold = 0.25f;
 static const float kDampeningFactor = 0.05f;
 static const int kIterations = 4000;
-static const float kDt = 0.01f;
+static const float kDt = 0.0075f;
 static const float kGravityConstant = 6.673e-11;   // gravitational constant
-static const int kNumBodies = 200000;
-static const float kMaxMass = 150.0f;
+static const int kNumBodies = 300000;
+static const float kMaxMass = 85.0f;
 static const int kSeed = 42;
 
 #endif  // EXAMPLE_BARNES_HUT_DYNAMIC_SOA_CONFIGURATION_H
