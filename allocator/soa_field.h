4b509dbe29acf7ce43b294bcdb0870e1e19cc4e3
Matthias Springer <me@matthiasspringer.de>
2019-10-02 13:15:44 +0900

    More comments.

--- allocator/soa_field.h
+++ allocator/soa_field.h
@@ -3,11 +3,30 @@
 
 #include "allocator/configuration.h"
 
+/**
+ * Contains various helper functions for extracting components from a fake
+ * pointer etc.
+ */
 struct PointerHelper {
+  /**
+   * Bit mask of bits used for encoding object slot IDs/indexes.
+   */
   static const uint8_t kObjectIdBitmask = 0x3F;
+
+  /**
+   * Bit mask of bits used for encoding block locations.
+   */
   static const uintptr_t kBlockPtrBitmask = 0xFFFFFFFFFFC0;
+
+  /**
+   * Bit mask of bits used for encoding memory location-related information.
+   */
   static const uintptr_t kMemAddrBitmask = kBlockPtrBitmask | kObjectIdBitmask;
 
+  /**
+   * Extracts the object slot ID from a fake pointer.
+   * @param obj Fake pointer
+   */
   __host__ __device__ static ObjectIndexT obj_id_from_obj_ptr(
       const void* obj) {
     uint8_t result =
@@ -17,12 +36,25 @@ struct PointerHelper {
     return reinterpret_cast<ObjectIndexT&>(result);
   }
 
+  /**
+   * Extracts the block location from a fake pointer. Note: Blocks are always
+   * located at such memory locations that the least significant
+   * kObjectIdBitmask many bits are zero. 
+   * @param obj Fake pointer
+   */
   __host__ __device__ static char* block_base_from_obj_ptr(const void* obj) {
     auto& ptr_base = reinterpret_cast<uintptr_t&>(obj);
     return reinterpret_cast<char*>(ptr_base & kBlockPtrBitmask);
   }
 
-  // Replace block base and object ID in ptr.
+  /**
+   * Replace block location and object slot ID in \p ptr. The other components
+   * of the fake pointer remain the same.
+   * @param ptr Fake pointer
+   * @param block_base New block location
+   * @param obj_id New object slot ID
+   * @tparam T Static type of the fake pointer
+   */
   template<typename T>
   __host__ __device__ static T* rewrite_pointer(T* ptr, void* block_base,
                                                 ObjectIndexT obj_id) {
@@ -37,6 +69,10 @@ struct PointerHelper {
     return reinterpret_cast<T*>(ptr_as_int);
   }
 
+  /**
+   * Extracts the type ID from a fake pointer.
+   * @param ptr Fake pointer
+   */
   __host__ __device__ static TypeIndexT get_type(const void* ptr) {
     auto& ptr_base = reinterpret_cast<uintptr_t&>(ptr);
     uint8_t type_id = ptr_base >> 56;  // Truncated.
@@ -44,29 +80,54 @@ struct PointerHelper {
   }
 };
 
-// Wrapper type for fields of SOA-structured classes. This class contains the
-// logic for calculating the data location of a field from an object
-// identifier.
+/**
+ * Field proxy type for fields of SOA-structured classes. This class contains
+ * the logic for calculating the data location of a field from a fake pointer,
+ * along with various operator overloadings for a seamless integration in C++.
+ * Note: This class contains most of the logic that is referred to as the
+ * "data layout DSL" in the paper.
+ * @tparam C Class in which the field is defined
+ * @tparam FieldIndex Index of the field
+ */
 template<typename C, int FieldIndex>
 class SoaField {
  private:
+  /**
+   * Type of field, as predeclared in the class definition.
+   */
   using T = typename SoaFieldHelper<C, FieldIndex>::type;
 
-  // Dummy field forces size of class to be zero.
+  /**
+   * By declaring an array of size 0, some compilers (e.g., GCC) treat this
+   * class as C++ size 0 (i.e., sizeof(SoaField<...>) == 0). This simplifies
+   * certain address computations and solves problems with zero initialization.
+   * Technically, this is a hack and violates the C++ standard. Check WPMVP
+   * paper for more details ("zero addressing mode").
+   */
   char _[0];
 
-  // Calculate data pointer from address.
+  /**
+   * Calculates the physical memory location of this field.
+   */
   __host__ __device__ T* data_ptr() const {
     // All SoaField<> have size 0. Offset of all fields is 1.
     auto ptr_base = reinterpret_cast<uintptr_t>(this) - 1;
     return data_ptr_from_obj_ptr(reinterpret_cast<C*>(ptr_base));
   }
 
+  /**
+   * Calculates the physical memory location of this field.
+   */
   __host__ __device__ volatile T* volatile_data_ptr() const {
     return data_ptr();
   }
 
  public:
+  /**
+   * Calculates the physical memory location of this field from a fake object
+   * pointer.
+   * @param obj Fake object pointer
+   */
   __host__ __device__ static T* data_ptr_from_obj_ptr(C* obj) {
     auto& ptr_base = reinterpret_cast<uintptr_t&>(obj);
 
@@ -92,6 +153,13 @@ class SoaField {
     return data_ptr_from_location(block_base, block_size, obj_id);
   }
 
+  /**
+   * Calculates the physical memory location of this field based on certain
+   * given components.
+   * @param block_base Address of the block
+   * @param obj_id Object slot ID
+   * @tparam BlockSize Block capacity
+   */
   template<int BlockSize>
   __host__ __device__ static T* data_ptr_from_location(char* block_base,
                                                        ObjectIndexT obj_id) {
@@ -114,47 +182,102 @@ class SoaField {
     return soa_array + obj_id;
   }
 
-  // Field initialization.
+  /**
+   * Field initialization.
+   * TODO: Force zero initialization of field? Seems like CUDA does not do this
+   * by default.
+   */
   __host__ __device__ SoaField() {}
 
+  /**
+   * Initializes the field. This constructor is invoked by field initializers.
+   * TODO: Initializer for field types is missing.
+   */
   __host__ __device__ explicit SoaField(const T& value) {
     *data_ptr() = value;
   }
 
-  // Implicit conversion operator for automatic conversion to base type.
+  /**
+   * Implicit conversion operator for automatic conversion to base type.
+   * I.e., unwrapping the proxy.
+   */
   __host__ __device__ operator T&() { return *data_ptr(); }
+
+  /**
+   * Implicit conversion operator for automatic conversion to base type.
+   * I.e., unwrapping the proxy.
+   */
   __host__ __device__ operator const T&() const { return *data_ptr(); }
 
-  // Explicitly get value. Just for better code readability.
+  /**
+   * Explicitly get value. Can be used internally. Just for better code
+   * readability.
+   */
   __host__ __device__ T& get() { return *data_ptr(); }
+
+  /**
+   * Explicitly get value. Can be used internally. Just for better code
+   * readability.
+   */
   __host__ __device__ const T& get() const { return *data_ptr(); }
 
+  /**
+   * Explicitly get value. Can be used internally. Just for better code
+   * readability.
+   */
   __host__ __device__ volatile T& volatile_get() {
     return *volatile_data_ptr();
   }
 
+  /**
+   * Explicitly get value. Can be used internally. Just for better code
+   * readability.
+   */
   __host__ __device__ const volatile T& volatile_get() const {
     return *volatile_data_ptr();
   }
 
-  // Custom address-of operator.
+  /**
+   * Returns the address of the physical memory location of this field.
+   */
   __host__ __device__ T* operator&() { return data_ptr(); }
+
+  /**
+   * Returns the address of the physical memory location of this field.
+   */
   __host__ __device__ const T* operator&() const { return data_ptr(); }
 
-  // Support member function calls.
+  /**
+   * This operator must be overloaded to support method calls if \p T is a
+   * class/struct type. Similar to std::unique_ptr.
+   */
   __host__ __device__ T& operator->() { return *data_ptr(); }
+
+  /**
+   * This operator must be overloaded to support method calls if \p T is a
+   * class/struct type. Similar to std::unique_ptr.
+   */
   __host__ __device__ const T& operator->() const { return *data_ptr(); }
 
-  // Dereference type in case of pointer type.
+  /**
+   * Dereferences the value of this field if \p T is a pointer type.
+   */
   __host__ __device__ typename std::remove_pointer<T>::type& operator*() {
     return **data_ptr();
   }
+
+  /**
+   * Dereferences the value of this field if \p T is a pointer type.
+   */
   __host__ __device__ typename std::remove_pointer<T>::type& operator*()
       const {
     return **data_ptr();
   }
 
-  // Array access in case of device array.
+  /**
+   * Allows this field to be used like an array.
+   * @param pos Array index
+   */
   template<typename U = T>
   __host__ __device__ typename std::enable_if<is_device_array<U>::value,
                                               typename U::BaseType>::type&
@@ -162,6 +285,11 @@ class SoaField {
     return (*data_ptr())[pos];
   }
 
+
+  /**
+   * Allows this field to be used like an array.
+   * @param pos Array index
+   */
   template<typename U = T>
   __host__ __device__ const typename std::enable_if<is_device_array<U>::value,
                                                     typename U::BaseType>
@@ -169,12 +297,20 @@ class SoaField {
     return (*data_ptr())[pos];
   }
 
-  // Assignment operator.
+  /**
+   * Assignment operator of assigning values of type \p T.
+   * @param value Value to be assigned
+   */
   __host__ __device__ T& operator=(const T& value) {
     *data_ptr() = value;
     return *data_ptr();
   }
 
+  /**
+   * Assignment operator of assigning values stored in another field.
+   * TODO: Assignment operator for other field types is missing.
+   * @param field Field to be assigned
+   */
   __host__ __device__ T& operator=(const SoaField<C, FieldIndex>& field) {
     *data_ptr() = field.get();
     return *data_ptr();
@@ -182,6 +318,9 @@ class SoaField {
 };
 
 
+/**
+ * Shortcut to SoaField.
+ */
 template<typename C, int FieldIndex>
 using Field = SoaField<C, FieldIndex>;
 
